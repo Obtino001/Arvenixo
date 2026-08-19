aea024c51d9bc5b1387b790f9e5ecbc6774217b1
Anonymous <devyasir001@gmail.com>
2026-08-19 12:39:55 +0500

    do

--- assets/global.js
+++ assets/global.js
@@ -435,7 +435,7 @@ class MenuDrawer extends HTMLElement {
       summary.addEventListener('click', this.onSummaryClick.bind(this))
     );
     this.querySelectorAll(
-      'button:not(.localization-selector):not(.country-selector__close-button):not(.country-filter__reset-button)'
+      'button:not(.localization-selector):not(.country-selector__close-button):not(.country-filter__reset-button):not([data-nlk-open]):not([data-nlk-back]):not(.nlk-back):not(.nlk-close):not(.nlk-row)'
     ).forEach((button) => button.addEventListener('click', this.onCloseButtonClick.bind(this)));
   }
 
